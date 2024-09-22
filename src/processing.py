0779100cfc3383cfb3707aae5d5c57e369d7f895
aleksey milyaev <trane94.milyaev@gmail.com>
2024-09-22 14:09:48 +0700

    add function sort_by_date and check: flake8, black, isort, mypy

--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,17 @@ def filter_by_state(operations: list, state: str = "EXECUTED") -> list:
             processed_operation_list.append(operation)
 
     return processed_operation_list
+
+
+def sort_by_date(operations: list, sort: bool = True) -> list:
+    """Функция сортировки операций по дате, второй аргумент задает порядок сортировки, по умолчанию - убывание"""
+    operation_sorted_list = []
+    date_list = []
+    for operation in operations:
+        date_list.append(operation["date"][:10])
+    date_list.sort(reverse=sort)
+    for item in date_list:
+        for operation in operations:
+            if item in operation["date"]:
+                operation_sorted_list.append(operation)
+    return operation_sorted_list
