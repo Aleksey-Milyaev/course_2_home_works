facb6b11648d396fdef67f47622fffbf0475567a
Aleksey_Milyaev <Trane94.milaev@yandex.ru>
2024-09-23 10:25:06 +0700

    add test_processing, fix processing and check: flake8, isort, black, mypy

---
+++ tests/test_processing.py
@@ -0,0 +1,36 @@
+import pytest
+
+from src.processing import filter_by_state, sort_by_date
+
+
+@pytest.fixture
+def operations():
+    """Данные по операциям"""
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+def test_filter_by_state(operations):
+    """Тест функции принимающей список операций и возвращающей новый список операций со значением 'state'."""
+    assert len(filter_by_state(operations)) == 2
+    assert filter_by_state() == "Данные небыли переданы"
+
+
+def test_sort_by_date(operations):
+    """Тест функции сортировки операций по дате, второй аргумент задает порядок сортировки, по умолчанию - убывание"""
+    assert sort_by_date(operations) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+    assert sort_by_date(operations, False) == [
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
