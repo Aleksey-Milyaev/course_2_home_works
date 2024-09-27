bc6c05c66e9fa2e1d7d0961ee15b43a4a63d5dee
Aleksey_Milyaev <Trane94.milaev@yandex.ru>
2024-09-28 00:41:11 +0700

    add logger masks, utils

--- src/masks.py
+++ src/masks.py
@@ -1,25 +1,42 @@
+import logging
+import os
+
+logger = logging.getLogger("masks")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs", "masks.log"), "w")
+file_formater = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_number: int = 0) -> str:
     """Функция маскировки номера банковской карты"""
+    logger.info(f"маскировка номера карты: {card_number}")
     if card_number == 0:
+        logger.info("Номер карты не введен")
         return "Номер карты не введен"
     card_number_string = str(card_number)
     if len(card_number_string) != 16:
+        logger.info("Номер карты не верен")
         return "Номер карты неверен"
     mask_card_number = card_number_string[:6] + "******" + card_number_string[-4:]
     card_number_format = (
         mask_card_number[:4] + " " + mask_card_number[4:8] + " " + mask_card_number[8:12] + " " + mask_card_number[12:]
     )
-
+    logger.info("Номер карты замаскирован")
     return card_number_format
 
 
 def get_mask_account(account_number: int = 0) -> str:
     """Функция маскировки номера банковского счета"""
+    logger.info(f"маскировка номера карты: {account_number}")
     if account_number == 0:
+        logger.info("Номер счета не введен")
         return "Номер счета не введен"
     account_number_string = str(account_number)
     if len(account_number_string) != 20:
+        logger.info("Номер счета не верен")
         return "Номер счета неверен"
     account_number_format = "**" + account_number_string[-4:]
-
+    logger.info("Номер счета замаскирован")
     return account_number_format
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,14 @@
 import json
+import logging
+import os
 from typing import Any
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs", "utils.log"), "w")
+file_formater = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
 
 
 def get_operations_list(path_to_json: str) -> Any:
@@ -8,6 +17,8 @@ def get_operations_list(path_to_json: str) -> Any:
     try:
         with open(path_to_json, encoding="UTF8") as file:
             content = file.read()
+            logger.info("Данные о транзакциях получены")
             return json.loads(content)
-    except Exception:
+    except Exception as ex:
+        logger.error(f"Произошла ошибка: {ex}")
         return []
