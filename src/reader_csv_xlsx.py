f3b2c78303d4f456904d322a176b1f63624cc987
Aleksey_Milyaev <Trane94.milaev@yandex.ru>
2024-09-28 23:03:29 +0700

    add test_reader_csv_xlsx, fix reader_csv_xlsx

--- src/reader_csv_xlsx.py
+++ src/reader_csv_xlsx.py
@@ -1,22 +1,28 @@
 import csv
-
-import pandas as pd
 import os
 
+import pandas as pd
 
-def read_operation_csv(file_path: str="") -> list:
+
+def read_operation_csv(file_path: str = "") -> list:
     """Функция считывания операций из файла.csv"""
-    with open(file_path, encoding="UTF8") as file:
-        reader_csv = csv.DictReader(file, delimiter=";")
-        transactions_dict = [row for row in reader_csv]
-    return transactions_dict
+    try:
+        with open(file_path, encoding="UTF8") as file:
+            reader_csv = csv.DictReader(file, delimiter=";")
+            transactions_dict = [row for row in reader_csv]
+        return transactions_dict
+    except FileNotFoundError:
+        return []
 
 
-def read_operation_xlsx(file_path: str) -> list:
+def read_operation_xlsx(file_path: str = "") -> list:
     """Функция считывания операций из файла.xlsx"""
-    transactions = pd.read_excel(file_path)
-    transactions_dict = transactions.to_dict(orient='records')
-    return transactions_dict
+    try:
+        transactions = pd.read_excel(file_path)
+        transactions_dict = transactions.to_dict(orient="records")
+        return transactions_dict
+    except FileNotFoundError:
+        return []
 
 
-print(read_operation_csv(os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "transactions.csv")))
+print(read_operation_xlsx(os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "transactions_excel.xlsx")))
--- tests/test_reader_csv_xlsx.py
+++ tests/test_reader_csv_xlsx.py
@@ -1,13 +1,44 @@
-import csv
 import os
+from unittest.mock import Mock, patch
 
-from src.reader_csv_xlsx import read_operation_xlsx, read_operation_csv
-from unittest.mock import patch
 import pandas as pd
+
+from src.reader_csv_xlsx import read_operation_csv, read_operation_xlsx
 
 
 @patch("csv.DictReader")
 def test_read_operation_csv(mock_read):
-    mock_read.return_value = ""
+    """Тест функции считывания операций из файла.csv"""
+    mock_read.return_value = iter(
+        [
+            {"id": 650703, "state": "EXECUTED", "amount": 16210.0},
+            {"id": 3598919, "state": "EXECUTED", "amount": 29740.0},
+        ]
+    )
 
-    assert read_operation_csv(os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "transactions.csv")) == {"Yes": 20, "No": 11}
+    assert read_operation_csv(
+        os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "transactions.csv")
+    ) == [
+        {"id": 650703, "state": "EXECUTED", "amount": 16210.0},
+        {"id": 3598919, "state": "EXECUTED", "amount": 29740.0},
+    ]
+    mock_read.assert_called_once()
+
+    assert read_operation_csv() == []
+
+
+def test_read_operation_xlsx():
+    """Тест функции считывания операций из файла.xlsx"""
+    mock_read = Mock(
+        return_value=pd.DataFrame(
+            {"id": [650703, 3598919], "state": ["EXECUTED", "EXECUTED"], "amount": [16210.0, 29740.0]}
+        )
+    )
+    pd.read_excel = mock_read
+    assert read_operation_xlsx(
+        os.path.join(os.path.dirname(os.path.dirname(__file__)), "data", "transactions_excel.xlsx")
+    ) == [
+        {"id": 650703, "state": "EXECUTED", "amount": 16210.0},
+        {"id": 3598919, "state": "EXECUTED", "amount": 29740.0},
+    ]
+    mock_read.assert_called_once()
